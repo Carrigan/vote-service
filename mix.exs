e22e95f437be654f7f61d9f4766c32844dd3a051
Brian Carrigan <brian.c.carrigan@gmail.com>
2016-05-03 20:04:42 -0400

    Fixed missing comma.

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Stv.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}
+     {:cowboy, "~> 1.0"},
      {:exrm, "~> 0.18.1"}]
   end
 
