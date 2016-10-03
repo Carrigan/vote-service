c8735e73d33184c5fafeece0b2d2ad3ed81a1ced
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-10-03 19:02:55 -0400

    Get the old parts working again.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,35 +1,19 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
-var node = document.getElementById('vote-box');
+//////////////
+// Elm Apps //
+//////////////
+let votingNode = document.getElementById('vote-box');
 let election = document.getElementById("election");
 
 function initializeVoteApp() {
-  let electionId = election.getAttribute("election-id")
+  let electionId = election.getAttribute("data-id");
   if($.cookie('election_' + electionId)) {
     window.location.href = "/results/" + electionId + "?fraud=true";
     return;
   }
 
-  var app = Elm.Vote.embed(node);
+  var app = Elm.Vote.embed(votingNode);
 
   app.ports.voteComplete.subscribe(function() {
     $.cookie('election_' + electionId, true);
@@ -37,9 +21,13 @@ function initializeVoteApp() {
   });
 }
 
-if (node) initializeVoteApp();
+if (votingNode) initializeVoteApp();
 
-export var VoteApp = {
+///////////////////
+// JQUERY API    //
+///////////////////
+
+const app = {
   get_election: function(election, success, failure) {
     $.ajax({
       type: "GET",
@@ -98,4 +86,158 @@ export var VoteApp = {
       error: failure
     });
   }
+}
+
+////////////////
+// New Screen //
+////////////////
+
+let newScreen = document.getElementById('create-new-election');
+
+if (newScreen) {
+  var close_link;
+  var election_id;
+
+  var close_success = function() {
+    window.location.href = "/results/" + election_id;
+  }
+
+  var close_failure = function() {
+    console.log("Add an error handler here please!");
+  }
+
+  $('#close-button').click(function() {
+    app.close_election(close_link, close_success, close_failure);
+  });
+
+  var success_handler = function(data) {
+    $('#loading').addClass('hidden');
+    $('#election_created').removeClass('hidden');
+
+    election_id = data.data.id;
+    close_link = data.data.close_url;
+    var election_link = '/vote/' + data.data.id;
+
+    $('#election-link').attr('href', election_link);
+    $('#election-link').text(election_link);
+  }
+
+  var failure_handler = function(data) {
+    $('#loading').addClass('hidden');
+    $('#create_new').removeClass('hidden');
+  }
+
+  var create_new_candidate = function() {
+    var container = $('#inputs_div');
+    container.append("<input type='text' class='form-control space-bottom'>");
+  }
+
+  create_new_candidate();
+
+  $('#more_candidates').click(function() {
+    create_new_candidate();
+  });
+
+  $('#new_button').click(function() {
+    $('#create_new').addClass('hidden');
+    $('#election_created').addClass('hidden');
+    $('#loading').removeClass('hidden');
+
+    var name = $('#election_name').val();
+    var seats = $('#seat_count').val();
+    var candidates = $.map($('#inputs_div :input'), function(input) { return $(input).val() });
+    app.make_election(candidates, name, seats, success_handler, failure_handler);
+  });
+}
+
+////////////////////
+// Results Screen //
+////////////////////
+
+election = document.getElementById("election-results");
+
+if (election) {
+  var electionId = election.getAttribute("data-id");
+
+  var query_string = function () {
+    // This function is anonymous, is executed immediately and
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+          // If first entry with this name
+      if (typeof query_string[pair[0]] === "undefined") {
+        query_string[pair[0]] = decodeURIComponent(pair[1]);
+          // If second entry with this name
+      } else if (typeof query_string[pair[0]] === "string") {
+        var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+        query_string[pair[0]] = arr;
+          // If third or later entry with this name
+      } else {
+        query_string[pair[0]].push(decodeURIComponent(pair[1]));
+      }
+    }
+    return query_string;
+  }();
+
+  if(query_string.voted) {
+    $('#thanks-for-voting').removeClass('hidden');
+  }
+
+  if(query_string.fraud) {
+    $('#voter-fraud').removeClass('hidden');
+  }
+
+  var request_results = function() {
+    app.get_election(electionId, success_handler, failure_handler);
+  }
+
+  var success_handler = function(data) {
+    if(data.data.status == "open") {
+      $('#vote_count').text(data.data.vote_count)
+      setTimeout(request_results, 5000);
+      return;
+    }
+
+    $('#results-box').removeClass('hidden');
+    $('#still-going').addClass('hidden');
+
+    $.each(data.data.candidates, function(i, candidate) {
+      if(candidate.winner) {
+        $('#winners').append("<li class='list-group-item'>" + candidate.name + "</li>");
+      }
+    });
+  }
+
+  var failure_handler = function(data) {
+    console.log("Replace this with a 404 please!");
+  }
+
+  request_results();
+}
+
+///////////
+// Index //
+///////////
+
+election = document.getElementById("election-index");
+
+if (election) {
+  var success_handler = function(data) {
+    if(data.data.length > 0) {
+      $('#election-box').removeClass('hidden');
+      $.each(data.data, function(i, entry) {
+        $('#current-elections').append("<tr><td><a href='/vote/" + entry.id +"'>" + entry.name +
+          "</a></td><td>" + entry.seat_count+ "</td><td>" + entry.created_at + "</td></tr>");
+      });
+    }
+  }
+
+  var failure_handler = function(data) {
+    console.log("Handle this.");
+  }
+
+  app.get_elections(success_handler, failure_handler);
 }
