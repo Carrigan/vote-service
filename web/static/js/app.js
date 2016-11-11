7887e7f7db327f4eb8d5e8f75cfbd3ec9add8557
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-11-11 16:54:54 -0500

    Remake vote module.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,21 +3,87 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
-var electionIndex = new Vue({
-  el: '#election-index',
-  data: {
-    elections: []
-  },
-  methods: {
-    renderElectionUrl: function(id) {
-      return '/vote/' + id;
-    },
-    loadElections: function() {
-      this.$http.get('/api/elections').then((response) => {
-        this.elections =response.data.data;
-      });
-    }
-  }
-});
+let election = document.getElementById("election");
 
-electionIndex.loadElections();
+if (document.getElementById("election-index")) {
+  var electionIndex = new Vue({
+    el: '#election-index',
+    data: {
+      elections: []
+    },
+    methods: {
+      renderElectionUrl: function(id) {
+        return '/vote/' + id;
+      },
+      loadElections: function() {
+        this.$http.get('/api/elections').then((response) => {
+          this.elections =response.data.data;
+        });
+      }
+    }
+  });
+
+  electionIndex.loadElections();
+}
+
+if (document.getElementById("ballot")) {
+  var ballotApp = new Vue({
+    el: '#ballot',
+    data: {
+      id: election.getAttribute("data-id"),
+      election: [],
+      currentRank: 0
+    },
+    methods: {
+      loadElection: function() {
+        this.$http.get('/api/elections/' + this.id).then((response) => {
+          let election = response.data.data;
+          election.candidates.forEach(candidate => candidate.rank = null);
+          this.election = election;
+        });
+      },
+
+      selectCandidate: function(selected) {
+        if (selected.rank !== null) {
+          let selectedRank = selected.rank;
+
+          this.election.candidates.forEach(candidate => {
+            if (candidate.rank > selectedRank) {
+              candidate.rank -= 1;
+            }
+
+            if (candidate.id === selected.id) {
+              candidate.rank = null;
+            }
+          });
+
+          this.currentRank -= 1;
+        } else {
+          selected.rank = this.currentRank;
+          this.currentRank += 1;
+        }
+      },
+
+      renderRank: function(rank) {
+        if (rank === null) return null;
+        return rank + 1;
+      },
+
+      submitVote: function() {
+        let entries = [];
+        this.election.candidates.forEach(candidate => {
+          if (candidate.rank !== null) {
+            entries.push({ candidate_id: candidate.id, rank: candidate.rank });
+          }
+        });
+
+        let body = { vote: { vote_entries: entries } }
+        this.$http.post('/api/elections/' + this.id + '/votes', body).then((response) => {
+          console.log(response);
+        });
+      }
+    }
+  });
+
+  ballotApp.loadElection();
+}
